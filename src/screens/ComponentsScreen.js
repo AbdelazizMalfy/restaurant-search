86b7b247f119a3350403e1c2ad157813fe38783b
Abdelaziz <abdelazizMalfy@gmail.com>
2019-12-01 13:09:46 +0100

    adding the first component

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text , StyleSheet , View , Button} from 'react-native';
+
+const ComponentsScreen = () => {
+    const name = 'abdelaziz'
+    return (
+        <View>
+            <Text style={styles.textStyle}> This is the ComponentsScreen </Text>
+            <Text style={styles.sectextStyle}>Abdelaziz</Text>
+            <Button title='LOVE' color='red'></Button>
+            <Text style={styles.subtextStyle}>Huda</Text>
+        </View>
+    )
+};  
+
+const styles = StyleSheet.create({
+    textStyle: {
+        textAlign:'center',
+        fontSize: 30,
+        padding:5
+    },subtextStyle: {
+        textAlign:'center',
+        padding:5
+    }
+
+
+})
+
+export default ComponentsScreen;
